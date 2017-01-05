fe7de231d2ea299182d95897edf2895ba7fd047f
Arpith Siromoney <arpith@feedreader.co>
2017-01-05 20:14:49 +0530

    add app icon

--- index.ios.js
+++ index.ios.js
@@ -9,7 +9,7 @@ import {
   View
 } from 'react-native';
 
-export default class Feedreader extends Component {
+export default class FeedReader extends Component {
   render() {
     const uri = 'https://feedreader.co';
     const {height, width} = Dimensions.get('window');
@@ -18,11 +18,11 @@ export default class Feedreader extends Component {
       <View style={{ flex: 1 }}>
         <WebView source={{ uri }} style={{ width, height }}/>
         <TabBarIOS>
-          <TabBarIOS.Item title={'\u2606'} style={{ fontSize: 10 }}/>
+          <TabBarIOS.Item title={'\u2606'} />
         </TabBarIOS>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('Feedreader', () => Feedreader);
+AppRegistry.registerComponent('FeedReader', () => FeedReader);

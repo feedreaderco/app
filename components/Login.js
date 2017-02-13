5cfca276e683c91c74306835c6b52b3dcda2bbd7
Arpith Siromoney <arpith@feedreader.co>
2017-02-13 17:33:11 +0530

    store login details in settings

--- components/Login.js
+++ components/Login.js
@@ -21,14 +21,36 @@ export default class Login extends Component {
     this.setState({modalOpen: false});
   }
 
+  updateSettingsState(key, value) {
+    const settings = this.state.settings;
+    console.log(settings);
+    settings[key] = value;
+    this.setState(settings);
+  }
+
   render() {
     return (
       <Modal animationType="fade" visible={this.state.modalOpen}>
-        <KeyboardAvoidingView behavior="padding">
-          <TextInput placeholder="Username" value={this.state.settings.username} />
-          <TextInput placeholder="Password" value={this.state.settings.password} />
+        <KeyboardAvoidingView 
+          behavior="padding"
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }} >
+          <TextInput 
+            placeholder="Username"
+            onChangeText={(text) => this.updateSettingsState('username', text)}
+            value={this.state.settings.username}
+            style={{flex: 1, borderColor: }} />
+          <TextInput
+            placeholder="Password"
+            onChangeText={(text) => this.updateSettingsState('password', text)}
+            value={this.state.settings.password}
+            style={{flex: 1}} />
+          <Button title="Login" onPress={this.login} />
         </KeyboardAvoidingView>
-        <Button title="Login" onPress={this.login} />
       </Modal>
     );
   }

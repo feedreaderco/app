57a523c4fdf794c3792faf2706bd3f3d82208551
Arpith Siromoney <arpith@feedreader.co>
2017-03-05 11:47:07 +0530

    style bottom toolbar

--- components/ToolBar.js
+++ components/ToolBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import BottomToolbar from 'react-native-bottom-toolbar';
 
-export default () => <BottomToolbar actions={[
-  { title: 'Add to Favorites', iconName: 'ios-heart-outline' },
-  { title: 'Share', iconName: 'ios-share-outline' },
-  { title: 'Read Later', iconName: 'ios-glasses-outline' },
-]} />;
+export default () => <BottomToolbar
+  wrapperStyle={{
+    backgroundColor: 'white',
+    borderTopWidth: 0
+  }}
+  actions={[
+    { title: 'Add to Favorites', iconName: 'ios-heart-outline' },
+    { title: 'Share', iconName: 'ios-share-outline' },
+    { title: 'Read Later', iconName: 'ios-glasses-outline' },
+  ]}
+/>;
